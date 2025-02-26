a7510ababf29249760effc44c52834a5c78f9238
Vincent Prins <vincent.prins@ivdnt.org>
2025-02-26 15:29:03 +0100

    column-info: added per file option

--- scripts/column-info.sh
+++ scripts/column-info.sh
@@ -1,29 +1,56 @@
 # usage:
-# ./scripts/column-info.sh --column 3
+# ./scripts/column-info.sh --column [number] --filter [regex] [--split-multi] [--per-file]
 
 # default values
 COLUMN=3
+SPLIT_MULTI=false
+FILTER="." # regex
+PER_FILE=false
 
 # get column number from args
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --column) COLUMN="$2"; shift ;;
+        --split-multi) SPLIT_MULTI=true ;;
+        --per-file) PER_FILE=true ;;
+        --filter) FILTER="$2"; shift ;;
         *) echo "Unknown parameter passed: $1" >&2; exit 1 ;;
     esac
     shift
 done
 
-# get all tags for that column from all files in the training-data folder
-TAGS=""
-for file in $(find training-data -name "*.tsv")
-do
-    TAGS="$TAGS
+# function to process tags
+process_tags() {
+    local TAGS="$1"
+    if [ $SPLIT_MULTI = true ]; then
+        TAGS=$(echo "$TAGS" | tr '|' '\n' | tr '+' '\n')
+    fi
+    TAGS=$(echo "$TAGS" | grep -E "[$FILTER]")
+
+    # don't echo when tags are empty
+    if [ -z "$TAGS" ]; then
+        echo "None found"
+        return
+    fi
+
+    echo "$TAGS" | sort | uniq -c | sort -nr
+}
+
+if [ $PER_FILE = true ]; then
+    for file in $(find training-data -name "*.tsv")
+    do
+        TAGS=$(cut -f $COLUMN $file)
+        echo "$file:"
+        process_tags "$TAGS"
+        echo
+    done
+else
+    TAGS=""
+    for file in $(find training-data -name "*.tsv")
+    do
+        TAGS="$TAGS
 $(cut -f $COLUMN $file)"
-done
-
-# some tags are so called multi tags, e.g. "A|B" or "A+B"
-# split them and add them to the list
-TAGS=$(echo "$TAGS" | tr '|' '\n' | tr '+' '\n')
-
-echo "Tags in column sorted by frequency:"
-echo "$TAGS" | sort | uniq -c | sort -nr
+    done
+    echo "Tags in column sorted by frequency:"
+    process_tags "$TAGS"
+fi
