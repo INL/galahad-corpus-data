27b2e49bd2bf22c696c2d6e285f8c638ce1dfd61
Vincent Prins <vincent.prins@ivdnt.org>
2025-12-16 17:13:16 +0100

    refactor token grouper

--- scripts/analyses/token_grouper.py
+++ scripts/analyses/token_grouper.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Callable
@@ -11,41 +12,39 @@ class TokenGrouper:
         self,
         out: Path,
         corpus: TsvCorpus,
-        group_by: Callable[[TsvWord], str],
-        sort_by: Callable[[TsvWord], str],
+        key_mapper: Callable[[TsvWord], str],
+        value_mapper: Callable[[TsvWord], str],
     ):
-        map: dict[str, dict[str, int]] = {}
+        map: dict[str, dict[str, int]] = defaultdict(lambda: defaultdict(int))
         for w in corpus.words:
-            key = group_by(w)
-            group = map.get(key, {})
-            sort_key = sort_by(w)
+            key = key_mapper(w)
             if w.group:
-                mwe = " ".join([sort_by(mw) for mw in w.mwe])
-                sort_key = f" MWE[{mwe}]"
+                value = f" MWE[{' '.join([value_mapper(m) for m in w.mwe])}]"
+            else:
+                value = value_mapper(w)
+            map[key][value] += 1
 
-            group[sort_key] = group.get(sort_key, 0) + 1
-            map[key] = group
         with out.open("w") as f:
-            for key, group in sorted(map.items(), key=lambda x: -sum(x[1].values())):
-                total_count = sum(group.values())
+            for key, values in sorted(map.items(), key=lambda x: -sum(x[1].values())):
+                total_count = sum(values.values())
                 f.write(f"{total_count} {key}\n")
 
                 mwe_items = {}
                 non_mwe_items = {}
 
-                for sort_key, count in sorted(
-                    group.items(), key=lambda x: (-x[1], x[0])
-                ):
-                    if "MWE[" in sort_key:
-                        mwe_items[sort_key] = count
+                for value, count in sorted(values.items(), key=lambda x: (-x[1], x[0])):
+                    if "MWE[" in value:
+                        mwe_items[value] = count
                     else:
-                        non_mwe_items[sort_key] = count
+                        non_mwe_items[value] = count
 
                 f.write(f"{sum(non_mwe_items.values())} [NOT-MWE]\n")
-                for sort_key, count in non_mwe_items.items():
-                    f.write(f"{count:>7}   {sort_key}\n")
+                for value, count in non_mwe_items.items():
+                    perc = (count / total_count) * 100
+                    f.write(f"{count:>7}{perc:>7.2f}% {value}\n")
 
                 f.write(f"{sum(mwe_items.values())} [MWE]\n")
-                for sort_key, count in mwe_items.items():
-                    f.write(f"{count:>7}   {sort_key}\n")
+                for value, count in mwe_items.items():
+                    perc = (count / total_count) * 100
+                    f.write(f"{count:>7}{perc:>7.2f}% {value}\n")
                 f.write("\n")
